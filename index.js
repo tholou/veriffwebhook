4c7cee253a1b3ed112fdeff36bbf76e54ca32b96
Tolu Eyinade <digital.3@brndwgn.com>
2019-11-14 16:07:57 +0100

    Added minor update

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 // Dependencies
-const fs = require('fs');
 const crypto = require('crypto');
-const fetch = require('node-fetch');
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv'); dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express().use(bodyParser.json()); // creates http server
 
 // Globals
-const API_TOKEN = process.env.API_TOKEN;
-const API_SECRET = process.env.API_SECRET;
+const VERIFF_API_TOKEN = process.env.VERIFF_API_TOKEN;
+const VERIFF_API_SECRET = process.env.VERIFF_API_SECRET;
 
-if (!API_TOKEN) throw('API_TOKEN environment variable is required');
-if (!API_SECRET) throw('API_SECRET environment variable is required');
+if (!VERIFF_API_TOKEN) throw('VERIFF_API_TOKEN environment variable is required');
+if (!VERIFF_API_SECRET) throw('VERIFF_API_SECRET environment variable is required');
 
 // app.get('/', (req, res) => {
 //     // check if verification token is correct
@@ -23,25 +22,9 @@ if (!API_SECRET) throw('API_SECRET environment variable is required');
 //     // return challenge
 //     return res.end(req.query.challenge);
 // });
-
-function generateSignature(payload, secret) {
-    if (payload.constructor === Object) {
-        payload = JSON.stringify(payload);
-    }
-
-    if (payload.constructor !== Buffer) {
-        payload = Buffer.from(payload, 'utf8');
-    }
-
-    const signature = crypto.createHash('sha256');
-    signature.update(payload);
-    signature.update(new Buffer.from(secret, 'utf8'));
-    return signature.digest('hex');
-}
-
 function isSignatureValid(data) {
-    const { signature, secret } = data;
-    let { payload } = data;
+    const {signature, secret} = data;
+    let {payload} = data;
 
     if (data.payload.constructor === Object) {
         payload = JSON.stringify(data.payload);
@@ -58,63 +41,52 @@ function isSignatureValid(data) {
 
 app.post('/test/hook', (req, res) => {
     const signature = req.get('x-signature');
-    const secret = API_SECRET;
+    const secret = VERIFF_API_SECRET;
     const payload = req.body;
-
-
     console.log('Received a webhook');
-    console.log('Generate signature:', generateSignature(payload, API_SECRET));
-    console.log('Validated signature:', isSignatureValid({ signature, secret, payload }));
-    // check if verification token is correct
-    // if (req.query.token !== token) {
-    //     return res.sendStatus(401);
-    // }
-
-    // print request body
+    console.log('Validated signature:', isSignatureValid({signature, secret, payload}));
     console.log(req.body);
 
-    // return a text response
-    const data = {
-        responses: [
-            {
-                type: 'text',
-                elements: ['Hi', 'Hello']
+    if (!isSignatureValid({signature, secret, payload})) {
+        res.status(401).json({
+            status_code: 401,
+            message: 'Unauthorized'
+        });
+    } else {
+        res.status(200).json({
+            status_code: 200,
+            data: {
+                verificationId: verificationId
             }
-        ]
-    };
-
-    res.json(data);
+        });
+    }
 });
 
+// Decision
 app.post('/test/notification', (req, res) => {
     const signature = req.get('x-signature');
-    const secret = API_SECRET;
+    const secret = VERIFF_API_SECRET;
     const payload = req.body;
-
-
     console.log('Received a webhook');
-    console.log('Generate signature:', generateSignature(payload, API_SECRET));
-    console.log('Validated signature:', isSignatureValid({ signature, secret, payload }));
-    // check if verification token is correct
-    // if (req.query.token !== token) {
-    //     return res.sendStatus(401);
-    // }
-
-    // print request body
+    console.log('Validated signature:', isSignatureValid({signature, secret, payload}));
     console.log(req.body);
 
-    // return a text response
-    const data = {
-        responses: [
-            {
-                type: 'text',
-                elements: ['Hi', 'Hello']
+    if (!isSignatureValid({signature, secret, payload})) {
+        res.status(401).json({
+            status_code: 401,
+            message: 'Unauthorized'
+        });
+    } else {
+        res.status(200).json({
+            status_code: 200,
+            data: {
+                verificationId: req.body.verification.id,
+                verificationStatus: req.body.verification.status,
+                reason: req.body.verification.reason,
             }
-        ]
-    };
-
-    res.json(data);
+        });
+    }
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port , () => console.log('[Veriff] Webhook is listening on port: ' + port));
+let port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Veriff Webhook is listening on port: ' + port));
